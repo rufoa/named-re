e7393d84400d95bfbe64096113479f676e8070ea
rufo <rufo@rufoa.com>
2013-02-09 23:27:40 +0000

    add midje plugin dep for travis

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject named-re "1.0.0"
 	:description "Named group support for regular expressions"
 	:dependencies [[org.clojure/clojure "1.4.0"] [reader-macros "1.0.1"]]
-	:profiles {:dev {:dependencies [[lein-midje "2.0.4"] [midje "1.4.0"]]}})
+	:profiles {:dev {:dependencies [[midje "1.4.0"]] :plugins [[lein-midje "2.0.4"]]}})
