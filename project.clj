54e4b25a0c4c5c91513b96c2a63c3bee6edeae33
rufo <rufo@rufoa.com>
2013-02-10 01:21:33 +0000

    license + url

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject named-re "1.0.0"
 	:description "Named group support for regular expressions"
+	:url "https://github.com/rufoa/named-re"
+	:license {:name "Eclipse Public License"
+	          :url "http://www.eclipse.org/legal/epl-v10.html"}
 	:dependencies [[org.clojure/clojure "1.4.0"] [reader-macros "1.0.1"]]
 	:profiles {:dev {:dependencies [[midje "1.5-alpha10"]] :plugins [[lein-midje "2.0.4"]]}})
